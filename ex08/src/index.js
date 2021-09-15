f3865a92ef7afaddecb242a1c281dead128d611d
dejo79 <dstefatic@gmail.com>
2021-09-15 22:33:29 +0200

    Added new files

---
+++ ex08/src/index.js
@@ -0,0 +1,12 @@
+function main(numOne, numTwo) {
+    var numOne;
+    var numTwo;
+    var sum = numOne + numTwo;
+    numOne = 15, 5, 150;
+    numTwo = 8, 28; 425;
+            return sum
+        }
+    console.log(main(15, 8));
+    console.log(main(5, 28));
+    console.log(main(150, 425));
+    module.exports = main;
