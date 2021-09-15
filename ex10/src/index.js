91585d7f273f7e8f0395f932d10c36906721c4ce
dejo79 <dstefatic@gmail.com>
2021-09-15 22:43:24 +0200

    Added new files

---
+++ ex10/src/index.js
@@ -0,0 +1,12 @@
+function main(numOne, numTwo) {
+    var numOne;
+    var numTwo;
+    var sum = numOne * numTwo;
+    numOne = 15, 25, 425;
+    numTwo = 3, 8; 10;
+            return sum
+        }
+    console.log(main(15, 3));
+    console.log(main(25, 8));
+    console.log(main(425, 10));
+    module.exports = main;
